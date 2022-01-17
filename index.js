fb2b84b23c595158eb01d8762c586902a2faad0a
Lindsey <lindsey.pregent@gmail.com>
2022-01-17 13:37:54 -0500

    commit tree 06585188556565188dfbcd234620c064a5e8bb07

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+//remove main DIV
+const mainDIV = document.querySelector("#main")
+mainDIV.remove();
+
+//create variable newHeader H1 with id of victory with textContent of "YOUR-NAME is the champion"
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.textContent = "Lindsey is the champion!"
